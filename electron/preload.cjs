255f70374d21f0e7ef91e0400b28847adad1043a
Avishai Tzabary <az0512124155azz@gmail.com>
2026-09-21 21:55:45 +0300

    Expose safe desktop connection APIs

--- electron/preload.cjs
+++ electron/preload.cjs
@@ -1,10 +1,13 @@
 const {contextBridge,ipcRenderer}=require('electron');
+
 contextBridge.exposeInMainWorld('desktopApi',{
   getStatus:()=>ipcRenderer.invoke('bridge:getStatus'),
   scanProviders:()=>ipcRenderer.invoke('bridge:scanProviders'),
   sendPrompt:(m)=>ipcRenderer.invoke('bridge:sendPrompt',m),
   configureRelay:(c)=>ipcRenderer.invoke('bridge:configureRelay',c),
-  apiChat:(payload)=>ipcRenderer.invoke('api:chat',payload),
+  listApiConnections:()=>ipcRenderer.invoke('api:listConnections'),
+  addApiConnection:(c)=>ipcRenderer.invoke('api:addConnection',c),
+  removeApiConnection:(id)=>ipcRenderer.invoke('api:removeConnection',id),
   captureScreens:()=>ipcRenderer.invoke('computer:captureScreens'),
   onStatus:(cb)=>{
     const h=(_e,s)=>cb(s);
