6998cd756df800bf211f15468de5b4473ba83428
Avishai Tzabary <az0512124155azz@gmail.com>
2026-09-22 12:32:43 +0300

    Expose Windows quick-chat controls

--- electron/preload.cjs
+++ electron/preload.cjs
@@ -15,6 +15,8 @@ contextBridge.exposeInMainWorld('desktopApi',{
   startSystemDictation:()=>ipcRenderer.invoke('dictation:start'),
   recognizeSystemDictation:()=>ipcRenderer.invoke('dictation:recognize'),
   speakText:(text)=>ipcRenderer.invoke('voice:speak',text),
+  setQuickChatEnabled:(value)=>ipcRenderer.invoke('quick-chat:setEnabled',value),
+  hideQuickChat:()=>ipcRenderer.invoke('quick-chat:hide'),
   captureAppshot:()=>ipcRenderer.invoke('appshot:capture'),
   onAppshot:(cb)=>listen('appshot-captured',cb),
   computerClick:(payload)=>ipcRenderer.invoke('computer:click',payload),
