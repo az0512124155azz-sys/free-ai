c472655ba6a39e8fbbcb3be61370b9ce55ac98d9
Avishai Tzabary <az0512124155azz@gmail.com>
2026-09-22 03:08:35 +0300

    Use the required public Android activity-result override

--- scripts/configure-android-deeplink.mjs
+++ scripts/configure-android-deeplink.mjs
@@ -75,7 +75,7 @@ import ee.forgr.capacitor.social.login.SocialLoginPlugin;
 
 public class MainActivity extends BridgeActivity implements ModifiedMainActivityForSocialLoginPlugin {
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
         if (requestCode >= GoogleProvider.REQUEST_AUTHORIZE_GOOGLE_MIN
